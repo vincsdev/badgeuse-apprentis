ad0f65d2a63dc721cafcceacd971e4c001d6207c
VincentS <vincent@casperlefantom.net>
2017-08-20 16:32:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -28,7 +28,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/...',
+    url='https://github.com/vinichou94/badgeuse-apprentis',
 
     # Author details
     author='Vincent S',
